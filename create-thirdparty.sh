4b49a60458db8b3cd9c73f648aa8f053d9a15f09
Stu Hood <stuhood@gmail.com>
2021-11-16 10:41:29 -0800

    Generated thirdparty BUILD files.

--- create-thirdparty.sh
+++ create-thirdparty.sh
@@ -2,16 +2,29 @@
 
 set -eu
 
+for t in $(rg -o 'addAkkaModuleDependency\([^),]*' build.sbt | awk -F'(' '{print $2}' | sort -u); do
+  target_name=$(echo "$t" | tr -d '"')
+  build_file_name="3rdparty/jvm/com/typesafe/akka/BUILD"
+
+  mkdir -p "$(dirname "${build_file_name}")"
+  echo "jvm_artifact(" >> "${build_file_name}"
+  echo "  name='${target_name}'," >> "${build_file_name}"
+  echo "  group='com.typesafe.akka'," >> "${build_file_name}"
+  echo "  artifact='${target_name}_2.13'," >> "${build_file_name}"
+  echo "  version='2.5.32'," >> "${build_file_name}"
+  echo ")" >> "${build_file_name}"
+  echo >> "${build_file_name}"
+done
+
 for t in $(rg -o '"[^"]*"\s*%%?.*%.*' project/Dependencies.scala | sort -u | tr ' ' '@'); do
   t=$(echo "$t" | tr '@' ' ')
   group=$(echo "$t" | awk '{print $1}')
-  dir="3rdparty/jvm/"$(echo "$group" | tr -d '"' | tr '.' '/')
-  build_file_name="${dir}/BUILD"
+  build_file_name="3rdparty/jvm/"$(echo "$group" | tr -d '"' | tr '.' '/')"/BUILD"
 
   target_name=$(echo "$t" | awk '{print $3}')
   version=$(echo "$t" | awk '{print $5}')
 
-  mkdir -p "$dir"
+  mkdir -p "$(dirname "${build_file_name}")"
   echo "jvm_artifact(" >> "${build_file_name}"
   echo "  name=${target_name}," >> "${build_file_name}"
   echo "  group=${group}," >> "${build_file_name}"
