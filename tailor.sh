932c259e9200cd0d222925c29001a17ed576387b
Stu Hood <stuhood@gmail.com>
2022-03-02 12:29:33 -0800

    Update to 2.10.0rc2.

--- tailor.sh
+++ tailor.sh
@@ -2,8 +2,8 @@
 set -eux
 
 # Run `tailor`.
-./pants_from_sources tailor
+./pants tailor
 
 # And regenerate the lockfile, which will determine what to include via the current implementation
 # of inference, and so will frequently need updating.
-./pants_from_sources coursier-resolve
+./pants generate-lockfiles --resolve=jvm-default
